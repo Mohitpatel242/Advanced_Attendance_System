f6e388b509a5b1a01b1c5f4eab60bba74a74a6fe
Mohit Patel <136880398+Mohitpatel242@users.noreply.github.com>
2024-07-29 10:20:08 +0530

    Update AddDatatoDatabase.py

--- AddDatatoDatabase.py
+++ AddDatatoDatabase.py
@@ -20,36 +20,6 @@ data = {
             "standing": "M",
             "year": 3,
             "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    "321654":
-        {
-            "name": "Murtaza Hassan",
-            "major": "Robotics",
-            "starting_year": 2017,
-            "total_attendance": 7,
-            "standing": "M",
-            "year": 4,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    "852741":
-        {
-            "name": "Emly Blunt",
-            "major": "Economics",
-            "starting_year": 2021,
-            "total_attendance": 12,
-            "standing": "B",
-            "year": 1,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    "963852":
-        {
-            "name": "Elon Musk",
-            "major": "Physics",
-            "starting_year": 2020,
-            "total_attendance": 7,
-            "standing": "G",
-            "year": 2,
-            "last_attendance_time": "2022-12-11 00:54:34"
         }
 }
 
